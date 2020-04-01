36991da7abef9edd78d0791cab38ffe3c3a509ba
BMSPOW <62870576+BMSPOW@users.noreply.github.com>
2020-04-01 09:27:29 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -24,18 +24,6 @@ namespace bmsmotor {
     const ALL_LED_OFF_L = 0xFC
     const ALL_LED_OFF_H = 0xFD
 
-    const STP_CHA_L = 2047
-    const STP_CHA_H = 4095
-
-    const STP_CHB_L = 1
-    const STP_CHB_H = 2047
-
-    const STP_CHC_L = 1023
-    const STP_CHC_H = 3071
-
-    const STP_CHD_L = 3071
-    const STP_CHD_H = 1023
-
 
     export enum Servos {
         S1 = 0x01,
@@ -129,32 +117,9 @@ namespace bmsmotor {
     }
 
 
-
-
     function stopMotor(index: number) {
         setPwm((index - 1) * 2, 0, 0);
         setPwm((index - 1) * 2 + 1, 0, 0);
-    }
-
-
-
-    /**
-     * Servo Execute
-     * @param index Servo Channel; eg: S1
-     * @param degree [0-180] degree of servo; eg: 0, 90, 180
-    */
-    //% blockId=bmsmotor_servo block="Servomotor|%index|degree %degree"
-    //% weight=100
-    //% degree.min=0 degree.max=180
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Servo(index: Servos, degree: number): void {
-        if (!initialized) {
-            initPCA9685()
-        }
-        // 50hz: 20,000 us
-        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
-        let value = v_us * 4096 / 20000
-        setPwm(index + 7, 0, value)
     }
 
 
@@ -205,6 +170,7 @@ namespace bmsmotor {
         MotorRun(motor2, speed2);
     }
 
+
     /**
      * Execute single motors with delay
      * @param index Motor Index; eg: M1A, M1B, M2A, M2B
@@ -243,6 +209,29 @@ namespace bmsmotor {
 
 
 
+    /**
+     * Servo Execute
+     * @param index Servo Channel; eg: S1
+     * @param degree [0-180] degree of servo; eg: 0, 90, 180
+    */
+    //% blockId=bmsmotor_servo block="Servomotor|%index|degree %degree"
+    //% weight=100
+    //% degree.min=0 degree.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+
+
+
+
     //% blockId=bmsmotor_rgbultrasonic block="Ultrasonic|pin %pin"
     //% weight=10
     export function RgbUltrasonic(pin: DigitalPin): number {
@@ -264,6 +253,8 @@ namespace bmsmotor {
 
         return Math.floor(ret * 9 / 6 / 58);
     }
+
+
 
     //% blockId=bmsmotor_holeultrasonicver block="Ultrasonic|pin %pin|version %v"
     //% weight=10
